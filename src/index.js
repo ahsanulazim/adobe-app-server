3c653aab92a99e3c41e3d43a9cb1e90a2262e6c5
Ahsanul Azim Plabon <ahsanulazim6@gmail.com>
2026-05-27 09:26:30 +0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import userRouter from "../router/user.route.js";
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://adobe-gnt.vercel.app"],
+    credentials: true,
+  }),
+);
+app.use(express.json());
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.use("/users", userRouter);
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
